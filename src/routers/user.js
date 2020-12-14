2973b195e5a44450f03a7a0d59321bb857f83d3f
pmonteiro <paulo.from.portugal@gmail.com>
2020-12-14 21:45:57 +0000

    Refactor app to user express.Router

---
+++ src/routers/user.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (error) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', async (req, res) => {
+
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/users/:id', async (req, res) => {
+    const bodyKeys = Object.keys(req.body)
+    const allowedFields = ['name', 'email', 'age', 'password']
+    const isValidOperation = bodyKeys.every((key) => allowedFields.includes(key))
+
+    if (!isValidOperation) {
+        return res.status(400).send({'error': 'Invalid body request'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+router.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
